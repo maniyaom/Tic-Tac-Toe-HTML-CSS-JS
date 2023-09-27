5ed2ef6d469914682d2ef41e0f140e1830f924e7
ommaniya70 <ommaniya01@gmail.com>
2023-09-27 20:37:17 +0530

    Added Bot

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -84,7 +84,7 @@ function winner(name) {
     }
     value = checkTie();
     if (value == true) {
-        return "Tie";
+        return "Draw";
     }
     else {
         return false;
@@ -119,14 +119,39 @@ function addCross(i){
     i.setAttribute("style", "cursor : revert");
 }
 
+function findBlankSquares(){
+    blankSquares = []
+    for (const i of squares){
+        if(i.innerHTML == ''){
+            blankSquares.push(i.getAttribute("id"))
+        }
+    }
+    return blankSquares;
+}
+
+function changeLevel(){
+    if(pvp == false && findBlankSquares().length == 9){
+        botLevel = document.getElementById("bot-level-selection-box").value;
+        document.querySelector("#easy-medium-hard-text").innerHTML = "Level : "+botLevel;
+        if(botLevel == "Easy")
+            botLevel = 0;
+        else if(botLevel == "Medium")
+            botLevel = (Math.floor(Math.random() * 2)) + 1;
+        
+        else if(botLevel == "Hard")
+            botLevel = 2;
+        console.log(botLevel);
+    }
+}
+
 function displayResult(name, value) {
     if (value == true) {
         document.querySelector(".turn").innerHTML = name + " Won the game.";
         document.querySelector(".pop-up").children[0].innerHTML = name + " Won.";
     }
     else {
-        document.querySelector(".turn").innerHTML = "Tie";
-        document.querySelector(".pop-up").children[0].innerHTML = "Tie";
+        document.querySelector(".turn").innerHTML = "Draw";
+        document.querySelector(".pop-up").children[0].innerHTML = "Draw";
     }
     alertBox = true;
     document.querySelector(".pop-up").style.display = "flex";
@@ -137,16 +162,127 @@ function displayResult(name, value) {
 }
 
 function restartGame(id) {
-    clickEffect(id)
-    document.querySelector(".pop-up").style.display = "none";
-    document.querySelector(".parent").style.opacity = "1";
-    document.querySelector(".header").style.opacity = "1";
-    document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
+    if(pvp != undefined){
+        clickEffect(id)
+        document.querySelector(".pop-up").style.display = "none";
+        document.querySelector(".parent").style.opacity = "1";
+        document.querySelector(".header").style.opacity = "1";
+        document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
+        player1 = true;
+        stopGame = false;
+        alertBox = false;
+        for (const i of squares) {
+            i.innerHTML = ""
+            i.removeAttribute("style");
+        }
+        if(pvp == false)
+            botLevel = document.getElementById("bot-level-selection-box").value;
+    }
+}
+
+function winPossibleInNextTurn(symbol,blankSquares){
+    let ele = '';
+        cols = document.querySelectorAll(".col");
+
+        // Checks for column
+        for (const i of cols){
+            let count = 0;
+            for (let j=0; j < i.children.length; j++){
+                if(i.children[j].innerHTML.includes(symbol)){
+                    count++;
+                }
+                else{
+                    ele = i.children[j];
+                }
+            }
+            if(count == 2){
+                if(blankSquares.includes(ele.getAttribute("id")))
+                    return ele;
+            }
+        }
+
+        // Checks for rows
+        for (let i = 0; i < 3; i++) {
+            let count = 0;
+            for (const j of cols) {
+                if (j.children[i].innerHTML.includes(symbol)) {
+                    count++;
+                }
+                else{
+                    ele = j.children[i];
+                }
+            }
+            if(count == 2){
+                if(blankSquares.includes(ele.getAttribute("id")))
+                    return ele;
+            }
+        }
+
+        // Checks For Diagonal Element
+        let i = 0;
+        for (let k = 0; k < 2; k++,i=2) {
+            count = 0
+            for (let j = 0; j < 3; j++) {
+                if (cols[j].children[i].innerHTML.includes(symbol)) {
+                    count++;
+                }
+                else{
+                    ele = cols[j].children[i];
+                }
+                
+                if(k == 0){
+                    i++;
+                }
+                else{
+                    i--;
+                }
+            }
+
+            if(count == 2){
+                if(blankSquares.includes(ele.getAttribute("id")))
+                    return ele;
+            }
+        }
+    return undefined;
+}
+
+function playrandom(blankSquares){
+    let x = Math.floor((Math.random() * (blankSquares.length - 1)) + 1);
+    
+    addCross(document.getElementById(blankSquares[x]));
     player1 = true;
-    stopGame = false;
-    alertBox = false;
-    for (const i of squares) {
-        i.innerHTML = ""
-        i.removeAttribute("style");
+}
+
+function runBot(level){
+    let blankSquares = findBlankSquares();
+    let a1 = document.querySelector("#a1");
+    let b2 = document.querySelector("#b2");
+    let c3 = document.querySelector("#c3");
+
+    if(level == 0){
+        playrandom(blankSquares);
+    }
+    else{
+        if(winPossibleInNextTurn(player2Symbol,blankSquares) == undefined){
+            if(winPossibleInNextTurn(player1Symbol,blankSquares) == undefined){
+                if(blankSquares.length == 8 && b2.innerHTML.includes(player1Symbol) && level == 2)
+                    addCross(a1);
+                else if(blankSquares.length == 6 && a1.innerHTML.includes(player2Symbol) && c3.innerHTML.includes(player1Symbol) && level == 2)
+                    addCross(document.querySelector("#c1"));
+                else
+                    playrandom(blankSquares);
+                player1 = true;
+            }
+            else{
+                let ele = winPossibleInNextTurn(player1Symbol,blankSquares);
+                addCross(ele);
+                player1 = true;
+            }
+        }
+        else{
+            let ele = winPossibleInNextTurn(player2Symbol,blankSquares);
+            addCross(ele);
+            player1 = true;
+        }
     }
 }
--- Scripts/script.js
+++ Scripts/script.js
@@ -5,6 +5,11 @@ let stopGame = true;  // Stop the game
 let submit = false;
 let alertBox = false;
 let toggleDark = false;
+let pvp = undefined; // Player Vs Player
+let player1Symbol = 'dot';
+let player2Symbol = 'cross-1';
+let botLevel;
+
 let squares = document.querySelectorAll(".square");
 const winningAudio = new Audio('media/winning-sound.wav');
 let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -13,20 +18,31 @@ if(colorScheme == true){
     toggle();
 }
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', event => {
+    if(event.key != 'Enter'){
+        event.preventDefault();
+    }
+});
 
 function clickOnSubmit() {
     player1Name = document.querySelector("#player1").value;
     player2Name = document.querySelector("#player2").value;
 
-    if (player1Name == "") {
+    if (player1Name == undefined || player1Name == '') {
         player1Name = "Player - 1";
     }
-    if (player2Name == "") {
+    if ((player2Name == '' || player2Name == undefined) && pvp == true) {
         player2Name = "Player - 2";
     }
-    document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
+    else if((player2Name == '' || player2Name == undefined) && pvp == false){
+        player2Name = "Bot";
+    }
 
-    if (submit == false) {
+    if(pvp != undefined)
+        document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
+
+    if (submit == false && pvp != undefined) {
         stopGame = false;
         document.querySelector("#submitBtn").innerHTML = "Restart";
         submit = true;
@@ -34,11 +50,43 @@ function clickOnSubmit() {
     else {
         restartGame("#submitBtn");
     }
+    player1Symbol = 'dot';
+    player2Symbol = 'cross-1';
+
+    if(pvp == false && submit == true){
+        botLevel = document.getElementById("bot-level-selection-box").value;
+        document.querySelector("#easy-medium-hard-text").innerHTML = "Level : "+botLevel;
+        if(botLevel == "Easy")
+            botLevel = 0;
+        else if(botLevel == "Medium")
+            botLevel = (Math.floor(Math.random() * 2)) + 1;
+        
+        else if(botLevel == "Hard")
+            botLevel = 2;
+    }
 }
 
 document.querySelector("#submitBtn").addEventListener("click", (e) => {
     e.preventDefault();
     clickOnSubmit();
+})
+
+document.querySelector("#pvp-btn").addEventListener("click",(event) => {
+    event.preventDefault();
+    document.querySelector("#user-choice-pvp-bot").setAttribute("style","display : none");
+    document.querySelector("#player1").setAttribute("style","display : block");
+    document.querySelector("#player2").setAttribute("style","display : block");
+    document.querySelector("#submitBtn").setAttribute("style","display : block");
+    pvp = true;
+})
+
+document.querySelector("#bot-btn").addEventListener("click",(event) => {
+    event.preventDefault();
+    document.querySelector("#user-choice-pvp-bot").setAttribute("style","display : none");
+    document.querySelector("#player1").setAttribute("style","display : block");
+    document.querySelector("#bot-level-selection-box").setAttribute("style","display : block");
+    document.querySelector("#submitBtn").setAttribute("style","display : block");
+    pvp = false;
 })
 
 document.querySelector('body').addEventListener("keypress",function(event){
@@ -70,13 +118,6 @@ for (const i of squares) {
 for (const i of squares) {
     i.addEventListener("click", (event) => {
         if (stopGame == false) {
-            if (player1Name == undefined) {
-                player1Name = "Player - 1";
-            }
-            if (player2Name == undefined) {
-                player2Name = "Player - 2";
-            }
-
             if (i.hasChildNodes()) {
                 if (i.firstChild.tagName == "SPAN") {
                     console.log("Skip")
@@ -87,32 +128,51 @@ for (const i of squares) {
                     addCircle(i)
                     player1 = false;
                     document.querySelector(".turn").innerHTML = "Turn : " + player2Name;
-                    let x = winner("dot");
-                    let y = winner("cross-1");
-                    if (x == true) {
-                        displayResult(player1Name, true);
-                    }
-                    else if (x == true) {
-                        displayResult(player2Name, true);
-                    }
-                    else if (y == "Tie") {
-                        displayResult("Tie", false);
-                    }
-                }
-                else {
-                    addCross(i);
-                    player1 = true;
-                    document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
-                    let x = winner("dot");
-                    let y = winner("cross-1");
+                    let x = winner(player1Symbol);
+                    let y = winner(player2Symbol);
                     if (x == true) {
                         displayResult(player1Name, true);
                     }
                     else if (y == true) {
                         displayResult(player2Name, true);
                     }
-                    else if (y == "Tie") {
-                        displayResult("Tie", false);
+                    else if (y == "Draw") {
+                        displayResult("Draw", false);
+                    }
+                    
+                    // Uncomment this section for play with bot
+                    if(x != true && y != true && y != "Draw" && pvp == false){
+                        document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
+                        runBot(level = botLevel);
+                    }
+                    
+                    x = winner(player1Symbol);
+                    y = winner(player2Symbol);
+                    if (x == true) {
+                        displayResult(player1Name, true);
+                    }
+                    else if (y == true) {
+                        displayResult(player2Name, true);
+                    }
+                    else if (y == "Draw") {
+                        displayResult("Draw", false);
+                    }
+                }
+
+                else if(pvp == true){
+                    addCross(i);
+                    player1 = true;
+                    document.querySelector(".turn").innerHTML = "Turn : " + player1Name;
+                    let x = winner(player1Symbol);
+                    let y = winner(player2Symbol);
+                    if (x == true) {
+                        displayResult(player1Name, true);
+                    }
+                    else if (y == true) {
+                        displayResult(player2Name, true);
+                    }
+                    else if (y == "Draw") {
+                        displayResult("Draw", false);
                     }
                 }
             }
